7b298ec6de3e2023df6ab979e1fd9ccc49c200fa
miguelmendes04 <miguel.mendes@sptech.school>
2022-06-06 00:09:35 -0300

    Alteração no script de BD, modelagem e inclusão dos slides para apresentação

--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -6,10 +6,16 @@ create database projetoValorant;
 
 use projetoValorant;
 
+create table Classe(
+idClasse int primary key auto_increment,
+nomeClasse varchar(20)
+);
+
 create table Agente(
 idAgente int primary key auto_increment,
 nomeAgente varchar(20),
-qtdVotos int
+qtdVotos int,
+fkClasse int
 );
 
 create table Mapa(
@@ -18,35 +24,40 @@ nomeMapa varchar(20),
 qtdVotos int
 );
 
-insert into Agente (nomeAgente, qtdVotos) values
-	('Astra', 0),
-	('Breach', 0),
-	('Brimstone', 0),
-	('Chamber', 0),
-	('Cypher', 0),
-	('Fade', 0),
-	('Jett', 0),
-	('Kayo', 0),
-	('Killjoy', 0),
-	('Neon', 0),
-	('Omen', 0),
-	('Phoenix', 0),
-	('Raze', 0),
-	('Reyna', 0),
-	('Sage', 0),
-	('Skye', 0),
-	('Sova', 0),
-	('Viper', 0),
-	('Yoru', 0);
-    
+insert into Classe values
+	(null, 'Controlador'),
+	(null, 'Duelista'),
+	(null, 'Iniciador'),
+	(null, 'Sentinela');
+
+
+insert into Agente (nomeAgente, qtdVotos, fkClasse) values
+	('Astra', 0, 1),
+	('Breach', 0, 3),
+	('Brimstone', 0, 1),
+	('Chamber', 0, 4),
+	('Cypher', 0, 4),
+	('Fade', 0, 3),
+	('Jett', 0, 2),
+	('Kayo', 0, 3),
+	('Killjoy', 0, 4),
+	('Neon', 0, 2),
+	('Omen', 0, 1),
+	('Phoenix', 0, 2),
+	('Raze', 0, 2),
+	('Reyna', 0, 2),
+	('Sage', 0, 4),
+	('Skye', 0, 3),
+	('Sova', 0, 3),
+	('Viper', 0, 1),
+	('Yoru', 0, 2);
+
 insert into Mapa (nomeMapa, qtdVotos) values
-('Ascent', 0),
-('Bind', 0),
-('Breeze', 0),
-('Fracture', 0),
-('Haven', 0),
-('Icebox', 0),
-('Range', 0),
-('Split', 0);
-    
-select * from agente;
+	('Ascent', 0),
+	('Bind', 0),
+	('Breeze', 0),
+	('Fracture', 0),
+	('Haven', 0),
+	('Icebox', 0),
+	('Range', 0),
+	('Split', 0);
